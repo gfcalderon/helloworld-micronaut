31f9e7202a688db6a5ae414d4e62e885245b0cac
Gerardo Fernández Calderón <gfcalderon@gmail.com>
2020-05-26 20:59:59 +0200

    Upgraded to Micronaut 2.0.0.M3 and Gradle 6.4.1

--- src/main/groovy/helloworld/client/HelloClient.groovy
+++ src/main/groovy/helloworld/client/HelloClient.groovy
@@ -1,7 +1,10 @@
 package helloworld.client
 
 import groovy.transform.CompileStatic
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
 import io.micronaut.http.client.annotation.Client
 import io.reactivex.Single
 
@@ -10,9 +13,11 @@ import io.reactivex.Single
 interface HelloClient {
 
     @Get( "/" )
+    @Consumes(MediaType.TEXT_PLAIN )
     Single hello()
-    
+
     @Get( "/{name}" )
+    @Consumes(MediaType.TEXT_PLAIN )
     Single hello( name )
 
 }
--- src/main/groovy/helloworld/client/IndexClient.groovy
+++ src/main/groovy/helloworld/client/IndexClient.groovy
@@ -1,15 +1,18 @@
 package helloworld.client
 
 import groovy.transform.CompileStatic
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.annotation.Client
 import io.reactivex.Single
- 
+
 @CompileStatic
 @Client( "/" )
 interface IndexClient {
 
     @Get( "/" )
+    @Consumes(MediaType.TEXT_PLAIN )
     Single index()
-    
+
 }
